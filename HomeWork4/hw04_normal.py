9c184e5f6ce3f6125a74dbe8fc36afcb6125396a
LexaGb <lexa_gb@mail.ru>
2018-10-30 17:54:34 +0500

    Выполнил задание 04_normal

--- HomeWork4/hw04_normal.py
+++ HomeWork4/hw04_normal.py
@@ -20,6 +20,30 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+print("Задание 1 Способ 1:")
+
+m = []
+s = ''
+
+for val in line:
+    if val.islower():
+        s += val
+    else:
+        if s:
+            m.append(s)
+        s = ''
+# чтобы зацепить еще последний символ, если он есть
+if s:
+    m.append(s)
+print(m)
+
+print("Задание 1 Способ 2:")
+
+import re
+
+pattern = '[a-z]+'
+m = re.findall(pattern, line)
+print(m)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,10 +69,50 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+print("Задание 2:")
+
+pattern = '[a-z]{2}([A-Z]+)[A-Z]{2}'
+m = re.findall(pattern, line_2)
+print(m)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+print("Задание 3:")
+
+import os
+import random
+
+path = os.path.join('text.txt')
+
+# запишем 2500 рандомных цифр в файл
+with open(path, 'w', encoding='UTF-8') as file:
+    for i in range(2500):
+        file.write(str(random.randint(0,9)))
+
+maxstr = ''
+# считаем из файла цифры в строку
+with open(path, 'r', encoding='UTF-8') as file:
+    s = file.read()
+    # Просто выведем строку
+    print(s)
+    # сначала найдем все возможжные последовательности подстрок с цифрой 0
+    # затем будем искать максимально длинную среди нолей
+    # на следующем цикле for найдем все возможжные последовательности подстрок с цифрой 1
+    # затем будем искать максимально длинную среди единиц длинне всех строк с нолями и т.д.
+    for i in range(10):
+        pattern = f'[{i}]+'
+        strbuf = re.findall(pattern, s)
+
+        for strel in strbuf:
+            if len(maxstr) < len(strel):
+                maxstr = strel
+
+print(maxstr)
+
+
 
