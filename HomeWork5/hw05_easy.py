1cfe19af69f9526d217a9b7008c658062e00f886
LexaGb <lexa_gb@mail.ru>
2018-11-08 18:40:45 +0500

    Переделал 05_easy с подсказками с разбора домашки

--- HomeWork5/hw05_easy.py
+++ HomeWork5/hw05_easy.py
@@ -5,37 +5,42 @@ import os
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
-print("Задание 1:")
+# print("Задание 1:")
 
 # Создадим директории 1-9
-for i in range(1,10):
-    name = f"dir_{i}"
-    if not os.path.exists(name):
-        os.mkdir(name)
+def CreateDirs():
+    for i in range(1,10):
+        name = f"dir_{i}"
+        if not os.path.exists(name):
+            os.mkdir(name)
 
 # Удалим директории 1-9
-for i in range(1,10):
-    name = f"dir_{i}"
-    if os.path.exists(name):
-        os.rmdir(name)
-
+def DeleteDirs():
+    for i in range(1,10):
+        name = f"dir_{i}"
+        if os.path.exists(name):
+            shutil.rmtree(name)
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
-print("Задание 2:")
-
-for val in os.listdir():
-    if os.path.isdir(val):
-        print(val)
+# print("Задание 2:")
+def ShowDir():
+    for val in os.listdir():
+        if os.path.isdir(val):
+            print(val)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 import sys
 import shutil
 
-print("Задание 3:")
+# print("Задание 3:")
 # Путь файла
-file = sys.argv[0]
-file = os.path.basename(file)
-shutil.copy(file, file + "_copy")
+
+def SelfCopy():
+    file = os.path.basename(__file__)
+    name, ext = os.path.splitext(file)
+    shutil.copy(file, f"{name}_copy{ext}")
+
+SelfCopy()
