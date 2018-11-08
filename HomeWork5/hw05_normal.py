442a054d04a36eb050fe3e5d67bd921afeecf717
Alexey Gab <lexa_gb@mail.ru>
2018-11-08 23:03:46 +0500

    Выполнил 05_normal

--- HomeWork5/hw05_normal.py
+++ HomeWork5/hw05_normal.py
@@ -13,3 +13,46 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import shutil
+
+def goToDir(path):
+    os.chdir(path)
+    pass
+
+def showDir(path = ''):
+    if path == '':
+        print(os.listdir())
+    else:
+        print(os.listdir(path))
+    pass
+
+def delDir(path):
+    shutil.rmtree(path)
+    pass
+
+def createDir(path):
+    os.mkdir(path)
+    pass
+
+
+commands = {
+    1: goToDir,
+    2: showDir,
+    3: delDir,
+    4: createDir
+}
+
+def printMenu():
+    print("Что нужно сделать?")
+    print("1 - перейти в указанную папку")
+    print("2 - показать содержимое папки")
+    print("3 - удалить папку")
+    print("4 - создать папку")
+
+while(True):
+    printMenu()
+    command = int(input("Код операции:"))
+    path = input("Имя папки:")
+    commands[command](path)
